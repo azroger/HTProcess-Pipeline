6c211727e640564d2a56fdeb065a2d816f5f7576
azroger <rogerab@email.arizona.edu>
2015-07-13 20:26:59 -0700

    patch for cuffmerge_htp.pl
    This works with cuffmerge_htp.pl to run the script in the plant
    Discovery Environment.

---
+++ cuffmerge_htp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#works with cuffmerge_htp.pl.20141015
+
+export PATH=$PATH:/usr/local2/cufflinks-2.2.0.Linux_x86_64:/usr/local2/rogerab/HTProcess/turtle
+/usr/local2/rogerab/HTProcess/cuffmerge_htp_script.pl $@
